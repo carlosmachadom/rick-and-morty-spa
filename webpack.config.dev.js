0cdfad8a13c35fdc1db71114a12c863b2b255a16
Carlos Machado <mcdario0223@gmail.com>
2023-09-13 09:39:01 -0500

    First SPA structure

---
+++ webpack.config.dev.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const DotEnv = require('dotenv-webpack');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: "./public/js/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].js",
+        clean: true
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            '@app': path.resolve(__dirname, 'src'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@templates': path.resolve(__dirname, 'src/templates'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@helpers': path.resolve(__dirname, 'src/helpers/'),
+            '@scripts': path.resolve(__dirname, 'src/scripts/'),
+            '@styles': path.resolve(__dirname, 'public/styles/'),
+            '@assets': path.resolve(__dirname, 'public/assets/'),
+            '@fonts': path.resolve(__dirname, 'public/assets/fonts/'),
+            '@images': path.resolve(__dirname, 'public/assets/images/')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[name].[hash][ext][query]',
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name].[hash][ext][query]',
+                },
+            },
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            inject: true,
+            template: './public/html/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].[contenthash].css'
+        }),
+        new DotEnv(),
+        new FaviconsWebpackPlugin('./public/assets/images/Favicon.png'),
+        new BundleAnalyzerPlugin()
+    ],
+    devServer: {
+        static:
+        {
+            directory: path.join(__dirname, "dist"),
+            watch: true,
+        },
+        watchFiles: path.join(__dirname, "./**"),
+        compress: true,
+        historyApiFallback: true,
+        port: 8080,
+        open: true,
+    },
+}
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const DotEnv = require('dotenv-webpack');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+
+module.exports = {
+    entry: "./public/js/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].js",
+        clean: true
+    },
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            '@app': path.resolve(__dirname, 'src'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@templates': path.resolve(__dirname, 'src/templates'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@helpers': path.resolve(__dirname, 'src/helpers/'),
+            '@scripts': path.resolve(__dirname, 'src/scripts/'),
+            '@styles': path.resolve(__dirname, 'public/styles/'),
+            '@assets': path.resolve(__dirname, 'public/assets/'),
+            '@fonts': path.resolve(__dirname, 'public/assets/fonts/'),
+            '@images': path.resolve(__dirname, 'public/assets/images/')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[name].[hash][ext][query]',
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name].[hash][ext][query]',
+                },
+            },
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            inject: true,
+            template: './public/html/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].[contenthash].css'
+        }),
+        new DotEnv(),
+        new FaviconsWebpackPlugin('./public/assets/images/Favicon.png'),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ],
+    },
+}
